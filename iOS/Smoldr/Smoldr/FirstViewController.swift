610d49a94673c9ff8328c491acebcb4bf1702ec4
AndrewSB <asbreckenridge@me.com>
2014-09-06 03:31:44 -0500

    Revert 38e004a..d180aa0
    This rolls back to commit 38e004af6694af195515fcf073b718d22245fc0b.

--- iOS/Smoldr/Smoldr/FirstViewController.swift
+++ iOS/Smoldr/Smoldr/FirstViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UITableViewController {
+class FirstViewController: UIViewController {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
