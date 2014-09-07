838f1385a543f59197d72f6138ec818f9e9edffb
AndrewSB <asbreckenridge@me.com>
2014-09-06 22:07:59 -0400

    added login logic. Another tab bar controller

---
+++ iOS/Smoldr/Smoldr/AirbnbTabBarItem.swift
@@ -0,0 +1,13 @@
+//
+//  AirbnbTabBarItem.swift
+//  Smoldr
+//
+//  Created by Andrew Breckenridge on 9/6/14.
+//  Copyright (c) 2014 asb. All rights reserved.
+//
+
+import UIKit
+
+class AirbnbTabBarItem: UITabBarItem {
+    
+}
---
+++ iOS/Smoldr/Smoldr/MainTabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  MainTabBarController.swift
+//  Smoldr
+//
+//  Created by Andrew Breckenridge on 9/6/14.
+//  Copyright (c) 2014 asb. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        if NSUserDefaults.standardUserDefaults().boolForKey("isLoggedIn") {
+            // Login
+            
+            // set nsusers
+            
+        }
+    }
+}
