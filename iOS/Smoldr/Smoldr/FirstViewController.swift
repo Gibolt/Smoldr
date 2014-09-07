52a65d5c1cbde172082f7a1ddcc22868d793b702
AndrewSB <asbreckenridge@me.com>
2014-09-07 03:44:03 -0400

    main tab is fucked up

--- iOS/Smoldr/Smoldr/FirstViewController.swift
+++ iOS/Smoldr/Smoldr/FirstViewController.swift
@@ -11,18 +11,7 @@ import UIKit
 class FirstViewController: UIViewController {
                             
     @IBOutlet var volcanoTableView: UITableView!
-
-    override func viewDidLoad() {
-        manualSegueCheck()
-    }
-
-    func manualSegueCheck() {
-        if !NSUserDefaults.standardUserDefaults().boolForKey("isLoggedIn") {
-            // go to login
-            //self.presentViewController(LoginSignupTabBarController(), animated: true, completion: nil)
-        }
-    }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
